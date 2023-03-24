8e376782e9e66231701448408b86a9a6b116d674
smc8501 <sofia.marijuancarreno15@gmail.com>
2023-03-24 20:48:38 +0000

    Set up Frontend

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # International Student Dashboard
+
+* `cd client`
+* `npm start` to run application
+
+<p>Website is in http://localhost:3000</p>
---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>International Interactive Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
