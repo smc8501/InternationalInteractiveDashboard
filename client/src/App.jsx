011c873a609cc6a91537efb9ac975616bfea1480
smc8501 <sofia.marijuancarreno15@gmail.com>
2023-03-25 09:24:43 +0000

    Create timeline point under timeline folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import TimelinePoint from './timeline/TimelinePoint.jsx';
 
 function App() {
   return (
     <div className="App">
       <h1>International Interactive Dashboard</h1>
+      <TimelinePoint />
     </div>
   );
 }
---
+++ client/src/timeline/TimelinePoint.jsx
@@ -0,0 +1,10 @@
+import './timelinePoint.css';
+
+
+
+function TimelinePoint() {
+    return(
+        <div class="timeline_point"></div>
+    );
+}
+export default TimelinePoint;
