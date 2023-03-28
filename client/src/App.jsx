4fbafc4bab46708881049066f81757597a71901b
smc8501 <sofia.marijuancarreno15@gmail.com>
2023-03-28 11:19:35 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+// Any jsx or css files that need to be linked here have to be imported.
+
 import './App.css';
 import TimelinePoint from './timeline/TimelinePoint.jsx';
+import RegisterForm from './registerPage/RegisterForm.jsx';
 
 function App() {
   return (
     <div className="App">
+      {/* This is the entry point for the frontend */}
       <h1>International Interactive Dashboard</h1>
-      <TimelinePoint />
+      {/* <TimelinePoint /> */}
+      <RegisterForm />
     </div>
   );
 }
