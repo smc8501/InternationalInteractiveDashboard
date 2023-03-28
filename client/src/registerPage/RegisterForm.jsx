98ed52bf19bb3a1e6154e9f7816207d37172ff4c
smc8501 <sofia.marijuancarreno15@gmail.com>
2023-03-28 11:19:03 +0100

    Create Register Form frontend

---
+++ client/src/registerPage/RegisterForm.jsx
@@ -0,0 +1,23 @@
+import PasswordField from '../Components/PasswordField';
+
+export default RegisterForm;
+function RegisterForm() {
+    return(
+        <form action="#">
+            <div class="container">
+                <h1>Register</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr></hr>
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="Enter Email" name="email" id="email" required></input>
+
+                    <PasswordField />
+
+                    <label for="psw-repeat"><b>Repeat Password</b></label>
+                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat"></input>
+            
+            </div>
+            
+        </form>
+    );
+}
