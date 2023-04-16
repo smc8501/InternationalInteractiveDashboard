3cab8533d8c52efb73a3ddac51e083fd98284c6c
smc8501 <sofia.marijuancarreno15@gmail.com>
2023-04-16 10:06:22 +0100

    Add Basic CSS and Material UI library of components to make it easier to design the application

--- client/src/pages/loginPage/LoginForm.jsx
+++ client/src/pages/loginPage/LoginForm.jsx
@@ -3,13 +3,15 @@
 import { useState } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
+import { Button } from '@mui/material';
+import { Input } from '@mui/joy';
+import './loginForm.css';
 
 
 function LoginForm() {
     const history=useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -26,18 +28,25 @@ function LoginForm() {
 
         <div className="Container">
             <form onSubmit={handleSubmit} >
-                <h1>Log In</h1>
-                <p>Please log in to continue your university application with us.</p>
-                <hr></hr>
-                <input type="text" onChange={(e) => {setUsername(e.target.value)}} placeholder="Username" id="username"/><br></br>
-                <input type="password" onChange={(e) => {setPassword(e.target.value)}} placeholder="Password" id="password"/><br></br>
-              
-                <button type="submit">Login</button>
-                <label>
-                    <b>Remember me</b>
-                    <input type="checkbox" name="remember"/>
-                </label>
-                <p>Don't have an account? Register <Link to="/registerform">here</Link></p> 
+                <div className="container-left">
+                    <h1>Log In</h1>
+                    <p>Please log in to continue your university application with us.</p>
+                    <hr></hr>
+                </div>
+                <div className="container-right">
+                    <Input type="text" placeholder="Username" id="username" onChange={(e) => {setUsername(e.target.value)}} size="sm" required/><br></br>
+                    {/* <input type="text" onChange={(e) => {setUsername(e.target.value)}} placeholder="Username" id="username"/><br></br> */}
+                    <Input type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} id="password" size="sm" required/><br></br>
+                    {/* <input type="password" onChange={(e) => {setPassword(e.target.value)}} placeholder="Password" id="password"/><br></br> */}
+                
+                    {/* <button type="submit">Login</button> */}
+                    <Button variant="contained">Log In</Button>
+                    <label>
+                        <b>Remember me</b>
+                        <input type="checkbox" name="remember"/>
+                    </label>
+                    <p>Don't have an account? Register <Link to="/registerform">here</Link></p> 
+                </div>
             </form>
         </div>
 
--- client/src/pages/registerPage/RegisterForm.jsx
+++ client/src/pages/registerPage/RegisterForm.jsx
@@ -2,6 +2,8 @@
 import {useNavigate, Link} from "react-router-dom";
 import { useState } from "react";
 import axios from 'axios';
+import { TextField, Button } from '@mui/material';
+import './registerForm.css';
 
 function RegisterForm() {
     const history=useNavigate();
@@ -11,7 +13,7 @@ function RegisterForm() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const errormessage="";
+    // const errormessage="";
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -28,23 +30,33 @@ function RegisterForm() {
     }
     return(
         <div className="container">
-            <form onSubmit={handleSubmit} action="POST">
-            <h1>Register</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr></hr>
-                {/* {registerInputs.map((input) => (
+            <div className="container-left">
+                <h1>Register</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr></hr>
+            </div>
+            <div className="container-right">
+                <form onSubmit={handleSubmit} action="POST">
+                    {/* {registerInputs.map((input) => (
                     <FormInput key={input.id} {...input} onChange={onChange}/>
-                ))} */}
-            <input type="text" onChange={(e) => { setFirstName(e.target.value) }} placeholder="First Name" id="firstName" required/><br></br>
-            <input type="text" onChange={(e) => { setLastName(e.target.value) }} placeholder="Last Name" id="lastName" required/><br></br>
-            <input type="text" onChange={(e) => { setUsername(e.target.value) }} placeholder="Username" errormessage="Username should be 3-16 characters and should not include any special character!" pattern={`^[A-Za-z0-9]{3,16}$`} id="username" required/><br></br><span>{errormessage}</span>
-            <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" errormessage="Input a valid email" id="email" required/><br></br><span>{errormessage}</span>
-            <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" errormessage="Password should be 8-20 characters and include at least 1 letter and 1 number." pattern={`^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%6&*]{8,20}$`} id="password" required/><br></br><span>{errormessage}</span>
-            <input type="password" placeholder= "Confirm Password" errormessage="Passwords do not match" pattern={password} required/><br></br><span>{errormessage}</span>
-            <button type="submit">Register</button>
-            <p>Already have an account? Log in <Link to="/loginform">here</Link></p> 
-            {/* Link in line 22 not working */}
-            </form>
+                    ))} */}
+                    <TextField label="First Name" onChange={(e) => { setFirstName(e.target.value) }} id="firstName" size="small" margin="dense"required/><br></br>
+                    {/* <input type="text" onChange={(e) => { setFirstName(e.target.value) }} placeholder="First Name" id="firstName" required/><br></br>  */}
+                    <TextField label="Last Name" onChange={(e) => { setLastName(e.target.value) }} id="lastName" size="small" margin="dense"required/><br></br>
+                    {/* <input type="text" onChange={(e) => { setLastName(e.target.value) }} placeholder="Last Name" id="lastName" required/><br></br> */}
+                    <TextField label="Username" onChange={(e) => { setUsername(e.target.value) }} id="username" size="small" margin="dense"pattern={`^[A-Za-z0-9]{3,16}$`} required/><br></br>
+                    {/* <input type="text" onChange={(e) => { setUsername(e.target.value) }} placeholder="Username" errormessage="Username should be 3-16 characters and should not include any special character!" pattern={`^[A-Za-z0-9]{3,16}$`} id="username" required/><br></br><span>{errormessage}</span> */}
+                    <TextField label="Email" onChange={(e) => { setEmail(e.target.value) }} id="email" size="small" margin="dense" required/><br></br>
+                    {/* <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" errormessage="Input a valid email" id="email" required/><br></br><span>{errormessage}</span> */}
+                    <TextField label="Password" type="password" onChange={(e) => { setPassword(e.target.value) }} size="small" margin="dense" pattern={`^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%6&*]{8,20}$`} id="password" required/><br></br>
+                    {/* <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" errormessage="Password should be 8-20 characters and include at least 1 letter and 1 number." pattern={`^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%6&*]{8,20}$`} id="password" required/><br></br><span>{errormessage}</span> */}
+                    <TextField label="Confirm Password"type="password" size="small" margin="dense"pattern={password}required/><br></br>
+                    {/* <input type="password" placeholder= "Confirm Password" errormessage="Passwords do not match" pattern={password} required/><br></br><span>{errormessage}</span> */}
+                    <Button variant="contained">Register</Button>
+                    {/* <button type="submit">Register</button> */}
+                    <p>Already have an account? Log in <Link to="/loginform">here</Link></p> 
+                </form>
+            </div>
         </div>
     );
 }
